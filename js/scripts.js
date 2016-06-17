004aefeb9d59a395cffb4e6848a13e89e2bd4523
Terri <TerriBradley@terri-bradleys-macbook-2.local>
2016-06-17 08:06:02 -0700

    create basic structure for html and some js

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var priceLarge = 12
+var price Small = 10
+var pizzaToppings =[];
+function Pizza (size, toppings) {
+  this.pizzSizes =size;
+  this.pizzaToppings = [toppings];
+  this.toppingTotals = toppungTotal
+}
+
+Pizza.finalPrice = function () {
+  if (this.pizzaSize === "large") {
+    priceLarge += this.PizzaToppings
+  }
+}
+
+Pizza.prototype.toppingPrice = function {
+  this.toppingTotals += this.pizzaToppings;
+  this.
+}
+
+
+
+
+
+
+(document).ready(function() {
+  $(form).submit (function() {
+    var inputtedPizzaSize = $(".pizza-size").val();
+    var inputtedPizzaToppings = parseInt($(".pizza-toppings").val());
+
+    var newPizza = new Pizza function(pizzaSize, pizzaToppings);
+    newPizza.finalPrice();
+  })
+})
