3646ffc7e9e37360a92eadbb044cfbffbf159e70
Terri Bradley <terribradley3@gmail.com>
2016-06-21 22:19:22 -0700

    fix js mistake to show cost and clear fields for new pizza order

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function Pizza(size, totalToppings) {
   this.totalToppings = totalToppings;
 }
 Pizza.prototype.priceCalculator = function() {
-  if (this.size === "small") {
+  if (this.pizzaSize === "small") {
     if (this.totalToppings <= 1) {
       return 12;
     } else if (this.totalToppings === 2) {
@@ -14,7 +14,7 @@ Pizza.prototype.priceCalculator = function() {
     } else {
       return 22;
     }
-  } else if (this.size === "medium") {
+  } else if (this.pizzaSize === "medium") {
     if (this.totalToppings <= 1) {
       return 16;
     } else if (this.totalToppings === 2) {
@@ -24,7 +24,7 @@ Pizza.prototype.priceCalculator = function() {
     } else {
       return 26;
     }
-  } else if (this.size === "large") {
+  } else if (this.pizzaSize === "large") {
     if (this.totalToppings <= 1) {
       return 20;
     } else if (this.totalToppings == 2) {
@@ -40,10 +40,20 @@ Pizza.prototype.priceCalculator = function() {
 $(document).ready (function() {
   $("#order-form").submit(function(event) {
     event.preventDefault();
+    // clearOrder();
     var size = $("input[name=size]:checked").val();
     var totalToppings = $("input:checkbox:checked").length;
     var pizza = new Pizza(size, totalToppings);
     var cost = pizza.priceCalculator();
-    $("#total").append("<p>" + "$" + cost + "</p>");
+    $("#total").show();
+    // debugger;
+    $("#show-cost").text(cost);
+    $("#new-order").click(function() {
+      $("input:radio").attr("checked", false);
+      $("input:checkbox").attr("checked", false);
+      $("#total").hide();
+    });
+
+
   });
 });
